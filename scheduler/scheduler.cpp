0a0445df5ac50e01cc15a861df5e466f263a2aa9
yshimizu20 <yukohshimizu@icloud.com>
2023-12-10 22:14:30 -0500

    hotfix2

--- scheduler/scheduler.cpp
+++ scheduler/scheduler.cpp
@@ -126,38 +126,38 @@ void scheduler(GlobalConfigLocal *currGlobalConfigLocal, pair<string, unordered_
     }
 }
 
-// int main() {
-//     ServiceConfigLocal *s1 = createService("nginx", 2);
-//     ServiceConfigLocal *s2 = createService("apache", 3);
-//     ServiceConfigLocal *s3 = createService("webapp", 1);
-//     ServiceConfigLocal *s4 = createService("loadbalancer", 1);
+int main() {
+    ServiceConfigLocal *s1 = createService("nginx", 2);
+    ServiceConfigLocal *s2 = createService("apache", 3);
+    ServiceConfigLocal *s3 = createService("webapp", 1);
+    ServiceConfigLocal *s4 = createService("loadbalancer", 1);
 
-//     vector<ServiceConfigLocal *> ss1 = {s1, s2};
-//     vector<ServiceConfigLocal *> ss2 = {s3, s4};
-//     ClusterConfigLocal *c1 = createCluster("us-east", ss1);
-//     ClusterConfigLocal *c2 = createCluster("us-west", ss2);
+    vector<ServiceConfigLocal *> ss1 = {s1, s2};
+    vector<ServiceConfigLocal *> ss2 = {s3, s4};
+    ClusterConfigLocal *c1 = createCluster("us-east", ss1);
+    ClusterConfigLocal *c2 = createCluster("us-west", ss2);
 
-//     vector<ClusterConfigLocal *> cs = {c1, c2};
-//     RegionConfigLocal *r1 = createRegion("us", cs);
+    vector<ClusterConfigLocal *> cs = {c1, c2};
+    RegionConfigLocal *r1 = createRegion("us", cs);
 
-//     vector<RegionConfigLocal *> rs = {r1};
-//     GlobalConfigLocal *currGlobalConfigLocal = createGlobal(rs);
+    vector<RegionConfigLocal *> rs = {r1};
+    GlobalConfigLocal *currGlobalConfigLocal = createGlobal(rs);
 
-//     cout << "Before updates:" << endl;
-//     printGlobalConfigLocal(*currGlobalConfigLocal);
-//     cout << endl;
+    cout << "Before updates:" << endl;
+    printGlobalConfigLocal(currGlobalConfigLocal);
+    cout << endl;
 
-//     pair<string, unordered_map<string, int>> update;
-//     update.first = "us";
+    pair<string, unordered_map<string, int>> update;
+    update.first = "us";
 
-//     unordered_map<string, int> service_map;
-//     service_map["nginx"] = 4;
-//     update.second = service_map;
+    unordered_map<string, int> service_map;
+    service_map["nnginx"] = 4;
+    update.second = service_map;
 
-//     scheduler(currGlobalConfigLocal, update);
+    scheduler(currGlobalConfigLocal, update);
 
-//     cout << "After updates:" << endl;
-//     printGlobalConfigLocal(*currGlobalConfigLocal);
+    cout << "After updates:" << endl;
+    printGlobalConfigLocal(currGlobalConfigLocal);
 
-//     return 0;
-// }
+    return 0;
+}
